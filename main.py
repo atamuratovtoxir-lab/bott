09ff872a983bfb5735ee980739fb7e37886eb445
atamuratovtoxir-lab <atamuratovtoxir@gmail.com>
2026-04-01 12:12:10 +0500

    Add GitHub Actions workflow for Python package with Conda

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = "8640980822:AAEjmVcoZ0X6PfK0uDA-cT3drqbU_9n6YBU"
@@ -8,58 +7,31 @@ SECRET_CODE = "72636288272783838"
 admins = set()
 users = set()
 
-logging.basicConfig(level=logging.INFO)
-
-# MENU
-def menu():
-    return ReplyKeyboardMarkup(
-        [["📰 Yangiliklar"]],
-        resize_keyboard=True
-    )
-
-# START
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
     users.add(user_id)
 
     if user_id in admins:
-        await update.message.reply_text(
-            "👮 Admin panelga xush kelibsiz!",
-            reply_markup=menu()
-        )
+        await update.message.reply_text("👮 Admin panel")
     else:
-        await update.message.reply_text(
-            "🇺🇿 WERTU | NEWS\n\n"
-            "📢 Siz yangiliklarni olasiz.",
-            reply_markup=menu()
-        )
+        await update.message.reply_text("🇺🇿 Botga xush kelibsiz!")
 
-# Xabarlar
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
     text = update.message.text
 
-    # Admin qilish kodi
     if text == SECRET_CODE and user_id not in admins:
         admins.add(user_id)
-
         await update.message.reply_text("✅ Siz ADMIN bo‘ldingiz!")
         return
 
-    # Agar admin bo‘lsa → broadcast
     if user_id in admins:
         for uid in users:
             try:
                 await context.bot.send_message(chat_id=uid, text=text)
             except:
                 pass
-        return
 
-    # Oddiy user
-    if text == "📰 Yangiliklar":
-        await update.message.reply_text("📰 Hozir yangiliklar yo‘q.")
-
-# MAIN
 async def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
@@ -67,6 +39,8 @@ async def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
 
     print("🚀 Bot ishga tushdi")
+
+    # ❗ MUHIM: asyncio.run YO‘Q!
     await app.run_polling()
 
 if __name__ == "__main__":
